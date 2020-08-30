fe11b8d2f826b2f6884f6df31e3d63e6be498265
KimMyeongJun <myeongjun222@naver.com>
2020-08-30 16:16:25 +0900

    codingtest

---
+++ cpp/CodingTest/CodingTest/ascii-code.cpp
@@ -0,0 +1,17 @@
+//문제 : https://www.acmicpc.net/problem/11654
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	char a;
+
+	printf("입력 : ");
+	cin >> a;
+	cout << static_cast<int>(a);
+
+
+	return 0;
+}
