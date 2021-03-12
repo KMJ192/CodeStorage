a22b712c3effc1d7b38e19a86db16e3c121efbc5
KimMyeongJun <myeongjun222@naver.com>
2021-03-12 14:36:41 +0900

    codingtest

---
+++ cpp/CodingTest/CodingTest/다리를지나는트럭.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+int TotalWeight(queue<int> bridge) {
+
+    return 0;
+}
+
+//큐를 사용하는 과정으로 수정
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    queue<int> bridge;
+
+    for (int i = 0; i < bridge.size(); i++) {
+        
+    }
+    return 0;
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    vector<int> truck_weight = { 7,4,5,6 };
+    cout << solution(2, 10, truck_weight);
+
+    return 0;
+}
--- cpp/CodingTest/CodingTest/전화번호목록.cpp
+++ cpp/CodingTest/CodingTest/전화번호목록.cpp
@@ -1,41 +1,48 @@
 #include<iostream>
 #include <string>
 #include <vector>
+#include<algorithm>
 
 using namespace std;
 
+//bool solution(vector<string> phone_book) {
+//    bool answer = true;
+//
+//    for (int i = 0; i < phone_book.size(); i++) {
+//        for (int j = i+1; j < phone_book.size(); j++) {
+//            if (i != j && i < j) {
+//                if(phone_book[i].length() <= phone_book[j].length()){
+//                    if (phone_book[j].find(phone_book[i]) != string::npos && phone_book[j].find(phone_book[i]) == 0)
+//                        return false;
+//                }
+//                else {
+//                    if (phone_book[i].find(phone_book[j]) != string::npos && phone_book[i].find(phone_book[j]) == 0)
+//                        return false;
+//                }
+//            }
+//        }
+//    }
+//
+//    return answer;
+//}
+
 bool solution(vector<string> phone_book) {
-    bool answer = true;
-
+    sort(phone_book.begin(), phone_book.end());
     for (int i = 0; i < phone_book.size(); i++) {
-        for (int j = 1; j < phone_book.size(); j++) {
-            if (i != j && i < j) {
-                //cout << phone_book[i]<< ", " << phone_book[j] << endl;
-                if(phone_book[i].length() <= phone_book[j].length()){
-                    if (phone_book[j].find(phone_book[i]) != string::npos && phone_book[j].find(phone_book[i]) == 0)
-                        return false;
-                }
-                else {
-                    if (phone_book[i].find(phone_book[j]) != string::npos && phone_book[i].find(phone_book[j]) == 0)
-                        return false;
-                }
-            }
-        }
+        if (i == phone_book.size() - 1) break;
+        if (phone_book[i + 1].find(phone_book[i]) != string::npos && phone_book[i + 1].find(phone_book[i]) == 0) return false;
     }
-
-    return answer;
+    return true;
 }
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    vector<string> phone_book = { "12","123","1235","567","88" };
+    vector<string> phone_book = { "123","456","789" };
 
-    //접두어 특정 문자 앞에 붙은거
-    //phone_book에서 접두어가 있을 경우 false, 없을 경우 true return
     cout << solution(phone_book) << "\n";
-
 
 	return 0;
 }
