8d5099866d65d2f0f687d112cdfa92c471ac3fd6
KimMyeongJun <myeongjun222@naver.com>
2021-03-17 21:36:16 +0900

    codingtest

--- cpp/CodingTest/CodingTest/N으로표현.cpp
+++ cpp/CodingTest/CodingTest/N으로표현.cpp
@@ -1,28 +1,78 @@
 #include<iostream>
 #include <string>
-#include <vector>
-#include <cassert>
+#include<vector>
 
 using namespace std;
 
-//N의 값을 이용한 사칙연산만을 이용하여 number를 표현하는데 N을 가장 적게 사용하는 방법 return
-//사칙연산 + - * /만 사용가능하며 / 연산에서 나머지는 무시함
-//최솟값이 8보다 클 경우 -1 return
-int solution(int N, int number) {
-    assert(1 <= N && N <= 9);
-    assert(1 <= number && number <= 32000);
-    int answer = 0;
+int global_n;
+int global_number;
+int tmp = 0;
+string gUnit;
 
-
-
-    if (answer > 8) {
-        return -1;
+vector<string> Combination(int number){
+    vector<string> com;
+    if(number == 1){
+            com.push_back("+");
+            com.push_back("-");
+            com.push_back("*");
+            com.push_back("/");
     }
+    else if (number == 2) {
+        com.push_back("+-");
+        com.push_back("+*");
+        com.push_back("+/");
+        com.push_back("-*");
+        com.push_back("-/");
+        com.push_back("*/");
+    }
+    else if (number == 3) {
+        com.push_back("+-*");
+        com.push_back("+-/");
+        com.push_back("-*/");
+    }
+    else {
+        com.push_back("+-*/");
+    }
+    return com;
+}
+
+//첫번째 Parameter => 이어붙일 숫자최소 단위
+//두번째 Parameter => 사칙연산 조합 단위
+//세번째 Parameter => 사칙연산 종류
+void Bfs(int n, int number, string s) {
+    cout << n << ", " << number << ", " << s << endl;
+    vector<string> unit;
+    for (int i = n; i <= 8; i++) {
+        for (int j = 1; j <= 4; j++) {
+            unit = Combination(j);
+            for (int n = 0; n < unit.size(); n++) {
+                Bfs(i, j, unit[n]);
+            }
+        }
+    }
+}
+
+
+//1개일때 총 8조각
+//
+int solution(int n, int number) {
+    int answer = -1;
+    if (n == number) return 1;
+
+    global_n = n;
+    global_number = number;
+    Bfs(1, 0, ""); // 숫자 최소단위와 사칙연산조합단위, 사칙연산 종류
     return answer;
 }
 
 int main() {
 
     solution(5, 12);
+
+    //string test = "1234";
+    //int nTest = stoi(test);
+    //nTest++;
+    //cout << nTest;
+
     return 0;
 }
