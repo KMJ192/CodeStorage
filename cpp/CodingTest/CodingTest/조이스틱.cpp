fe093dfb978597771dc0da5d0a7121077466f33e
KimMyeongJun <myeongjun222@naver.com>
2021-03-30 14:53:39 +0900

    codingtest

---
+++ cpp/CodingTest/CodingTest/조이스틱.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+//위로올리면 다음 알파벳
+//아래로 내리면 이전 알파벳
+//왼쪽이랑 오른쪽은 머임??
+
+int solution(string name) {
+    int answer = 0;
+    return answer;
+}
+
+int main() {
+
+    int test;
+    test = 'Y';
+    cout << test << endl;
+
+    return 0;
+}
--- cpp/CodingTest/CodingTest/큰수만들기.cpp
+++ cpp/CodingTest/CodingTest/큰수만들기.cpp
@@ -1,35 +1,63 @@
 #include<iostream>
-#include <string>
 #include<vector>
+#include<queue>
 #include<algorithm>
 
 using namespace std;
 
-string solution(string number, int k) {
-    char maxVal;
-    int count = k, index = 0;
-    string answer = "";
-    while (answer.size() < number.size() - k) {
-        maxVal = 0;
-        for (int i = index; i < number.size() - count; i++) {
-            cout << number[i];
-            if (maxVal < number[i]) {
-                index = i + 1;
-                maxVal = number[i];
+priority_queue<string> total;
+vector<vector<int>> temp;
+
+void Permutation(string object, int k) {
+    if (k >= object.size()) return;
+    vector<int> tmp;
+    for (int i = 0; i < k; i++) {
+        tmp.push_back(i);
+    }
+
+    for (int i = 0; i < object.size(); i++) {
+        
+    }
+}
+
+string ExtractString(string object, vector<int> position) {
+    if (object.size() < position.size()) return "";
+    int cnt = 0;
+    string tmp;
+
+    for (int n = 0; n < object.size(); n++) {
+        if (cnt < position.size()) {
+            if (n == position[cnt]) {
+                cnt++;
+                continue;
             }
         }
-        cout << endl;
-        answer += maxVal;
-        count--;
+        tmp = tmp + object[n];
     }
-    cout << answer << endl;
+    return tmp;
+}
 
+void Dfs(string number, int k, int last) {
+    if (last == number.size() - k) return;
+    
+
+
+    string tmp;
+}
+
+string solution(string number, int k) {
+
+    Dfs(number, k, 0);
+
+    string answer = "";
     return answer;
 }
 
 int main() {
-    
+
     solution("4177252841", 4);
+    vector<int> test = { 1, 2, 4 };
+    //ExtractString("1231234", test);
 
     return 0;
 }
