f740944013a0165dd3fa8100116232cf8d7f5515
KimMyeongJun <myeongjun222@naver.com>
2021-04-04 19:47:36 +0900

    codingtest

---
+++ cpp/CodingTest/CodingTest/DesignHashSet.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+class MyHashSet {
+private:
+
+public:
+    MyHashSet() {
+
+    }
+
+    void add(int key) {
+
+    }
+
+    void remove(int key) {
+
+    }
+
+    bool contains(int key) {
+
+    }
+};
+
+int main() {
+
+    MyHashSet hashset;
+
+    return 0;
+}
