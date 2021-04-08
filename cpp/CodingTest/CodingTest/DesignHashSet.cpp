87363fea50d1203ad146274101969c8ce16d55e8
KimMyeongJun <myeongjun222@naver.com>
2021-04-08 12:18:48 +0900

    codingtest

--- cpp/CodingTest/CodingTest/DesignHashSet.cpp
+++ cpp/CodingTest/CodingTest/DesignHashSet.cpp
@@ -1,37 +1,46 @@
 #include<iostream>
-#include<vector>
+#include<cassert>
+#include<list>
 
 using namespace std;
 
 class MyHashSet {
-private:
-    int key;
 public:
-    /** Initialize your data structure here. */
-    MyHashSet() {
-        key = 0;
-    }
+    //saved key
+    list<int> set;
+    MyHashSet() {}
 
+    //add key
     void add(int key) {
-
+        if (contains(key) == false) set.push_back(key);
+        else throw "existe key";
     }
 
+    //remove key
     void remove(int key) {
-
+        set.remove(key);
     }
 
-    /** Returns true if this set contains the specified element */
+    //contains key
     bool contains(int key) {
-
+        for (list<int>::iterator it = set.begin(); it != set.end(); it++) {
+            if (*it == key) return true;
+        }
+        return false;
     }
-
-    ~MyHashSet(){}
+    ~MyHashSet() {
+        set.clear();
+    }
 };
+
 
 int main() {
 
     MyHashSet myHashSet;
     myHashSet.add(1);
+    myHashSet.add(2);
+    //myHashSet.remove(2);
+    
 
 	return 0;
 }
