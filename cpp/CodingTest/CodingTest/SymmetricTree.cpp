39c83673d9853e0d0e6edf75ce4f3b596ed753a5
KimMyeonJun <myeongjun222@naver.com>
2021-04-25 10:18:35 +0900

    codingtest

--- cpp/CodingTest/CodingTest/SymmetricTree.cpp
+++ cpp/CodingTest/CodingTest/SymmetricTree.cpp
@@ -18,67 +18,67 @@ struct TreeNode {
 class Solution {
 private:
     queue<TreeNode*> q;
-    vector<string> v;
+    vector<TreeNode*> compare;
+    vector<int> v;
 public:
-    bool isSymmetric(TreeNode* root) {
-        
-        TreeNode* tmp;
-        int startPnt = 0;
+    Solution() {}
+    ~Solution() {}
 
+    int commonLevel(TreeNode* node) {
+        int cnt = 0;
+        if (node->left) {
+            compare.push_back(node->left);
+            cnt++;
+        }
+        if (node->right) {
+            compare.push_back(node->left);
+            cnt++;
+        }
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        TreeNode* tmp;
         tmp = root;
         q.push(tmp);
-        v.push_back(to_string(tmp->val));
-        while (!q.empty()) {
+        compare.push_back(tmp);
+        while (!q.empty()) {            
+            int lCnt = 0;
             tmp = q.front();
             q.pop();
             if (tmp->left != nullptr) {
+                lCnt++;
                 q.push(tmp->left);
-                v.push_back(to_string(tmp->left->val));
-            }
-            else {
-                v.push_back("null");
+                compare.push_back(tmp->left);
             }
             if (tmp->right != nullptr) {
+                lCnt++;
                 q.push(tmp->right);
-                v.push_back(to_string(tmp->right->val));
+                compare.push_back(tmp->right);
+            }
+            v.push_back(lCnt);
+        }
+        int next = 0;
+        int init = 0;
+        int cnt = 0;
+        bool toggle = false;
+        for (int i = 0; i < compare.size(); i++) {
+            if (toggle == false) {
+                init = next = v[i] + i;
+                toggle = true;
+                cnt = 0;
             }
             else {
-                v.push_back("null");
+                cnt += v[i];
+            }
+            cout << compare[i]->val << " ";
+            cout << v[i] << endl;
+            if (next == i) {
+                cout << "test" << endl;
+                toggle = false;
+                cout << "간선의 수 : " << cnt << endl;
             }
         }
-
-        int cnt = 2;
-        int pos = 1;
-        vector<string> test;
-        for (int i = 1; i < v.size(); i++) {
-            if (i == pos) {
-                test.clear();
-                //pos에 다음 포지션 입력
-                if (cnt == 2) {
-                    pos = cnt + 1;
-                    cnt *= 2;
-                }
-                else {
-                    pos = cnt + pos;
-                    cnt *= 2;
-                }
-
-                //다음을 가리키고 있는 위치값이 v보다 클 경우 종료
-                if (pos > v.size() - 1) {
-                    break;
-                }
-
-                for (int j = i; j < pos; j++) {
-                    test.push_back(v[j]);
-                }
-                for (int j = 0; j < test.size() / 2; j++) {
-                    cout << test[j] << test[test.size() - 1 - j] << endl;
-                    if (test[j] != test[test.size() - 1 - j]) {
-                        return false;
-                    }
-                }
-            }
-        }
+        
 
         delete tmp;
         return true;
@@ -88,18 +88,58 @@ public:
 
 int main() {
     //root노드
-    TreeNode* root = new TreeNode(1);
+    TreeNode* root = new TreeNode(2);
 
-    root->left = new TreeNode(2);
-    root->right = new TreeNode(2);
+    root->left = new TreeNode(3);
+    root->right = new TreeNode(3);
 
-    root->left->right = new TreeNode(3);
-    root->right->left= new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->right->right = new TreeNode(4);
+    
+    root->left->left->right = new TreeNode(5);
+    root->right->right->left = new TreeNode(5);
+
+    root->left->left->right->right = new TreeNode(6);
+    root->right->right->left->left = new TreeNode(6);
+
+    root->left->left->right->right->left = new TreeNode(7);
+    root->left->left->right->right->right = new TreeNode(8);
+    root->right->right->left->left->left = new TreeNode(8);
+    root->right->right->left->left->right = new TreeNode(7);
+
+
+    root->left->left->right->right->left->left = new TreeNode(9);
+    root->left->left->right->right->left->right = new TreeNode(0);
+    root->left->left->right->right->right->left = new TreeNode(0);
+    root->left->left->right->right->right->right = new TreeNode(1);
+    root->right->right->left->left->left->left = new TreeNode(1);
+    root->right->right->left->left->left->right = new TreeNode(0);
+    root->right->right->left->left->right->left = new TreeNode(0);
+    root->right->right->left->left->right->right = new TreeNode(9);
+
 
     Solution s;
-    cout << s.isSymmetric(root) << endl;
+    s.isSymmetric(root);
 
     delete root;
 
     return 0;
 }
+
+//Input: 
+//Output: return value
+//Constraints :
+// 1) num of nodes,
+// 2) node value = pos or neg
+//Edge Case :
+// 1) tree == = null, return root;
+//
+//brute force : naive
+//  DFS or BFS or iteratively or recursive
+//  time, space
+//optimal solution
+//  algorithmand Data structure
+//  time, space
+//
+//code :
+//  function
