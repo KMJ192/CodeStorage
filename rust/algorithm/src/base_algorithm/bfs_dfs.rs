1596d200e86d2026b19cf37cd53e3f50027fb815
KimMyeonJun <myeongjun222@naver.com>
2021-04-27 00:17:37 +0900

    pj1

--- rust/algorithm/src/base_algorithm/bfs_dfs.rs
+++ rust/algorithm/src/base_algorithm/bfs_dfs.rs
@@ -27,48 +27,58 @@ impl Solution {
     }
 }
 
-
-fn bfs(){
-
+enum List{
+  Cons(i32, Rc<List>),
+  Nil,
 }
-fn dfs(){
-
-}
-
-use std::io;
-use std::str::FromStr;
+use List::{Cons, Nil};
 
 pub fn run(){ 
-    //let mut root : TreeNode= TreeNode{val : 1, left : None, right: None};
-    let mut root : TreeNode;
-    let mut tmp : Rc<RefCell<TreeNode>>;
-    //root node 입력
-    // root = TreeNode{val : 2, left : None, right : None};
-    // tmp = Rc::new(RefCell::new(TreeNode{val : 3, left : None, right: None}));
-    // root.left = Some(tmp);
-    // tmp = Rc::new(RefCell::new(TreeNode{val : 3, left : None, right: None}));
-    // root.right = Some(tmp);
-    // println!("{:#?}", root.left);
-    
-    //직접 넣자
-    //*를 입력할 때 까지 반복
-    //left, right를 각각 입력
-    loop{
-      let mut node_val = String::new();
-      io::stdin().read_line(&mut node_val).expect("error");
-      if node_val == "*"{
-        break;
+  //let mut root : Option<Rc<RefCell<TreeNode>>>;
+  //let mut tmp : Rc<RefCell<TreeNode>>;
+  //root node 입력
+  //tmp = Rc::new(RefCell::new(TreeNode{val : 1, left : None, right: None}));
+  //Solution::is_symmetric();
+
+  let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+  println!("{}", Rc::strong_count(&a));
+  let b = Cons(3, Rc::clone(&a));
+  println!("{}", Rc::strong_count(&a));
+  let c = Cons(2, Rc::clone(&a));
+  println!("{}", Rc::strong_count(&a));
+}
+
+pub trait Messenger {
+  fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: 'a + Messenger> {
+  messenger: &'a T,
+  value: usize,
+  max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+  where T: Messenger {
+  pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+      LimitTracker {
+          messenger,
+          value: 0,
+          max,
       }
-      let test : i32 = node_val.parse::<i32>().unwrap();
-      //let test : i32 = FromStr::from_str(node_val).unwrap();
-      println!("{}", test);
-  
-    }
-    // tmp = Rc::new(RefCell::new(TreeNode{val : 4, left : None, right: None}));
-    // root.left = Some(tmp);
-    // tmp = Rc::new(RefCell::new(TreeNode{val : 4, left : None, right: None}));
-    // root.right = Some(tmp);
-    
-    //println!("{:#?}", root);
-    //Solution::is_symmetric();
   }
+
+  pub fn set_value(&mut self, value: usize) {
+      self.value = value;
+
+      let percentage_of_max = self.value as f64 / self.max as f64;
+
+      if percentage_of_max >= 0.75 && percentage_of_max < 0.9 {
+          self.messenger.send("Warning: You've used up over 75% of your quota!");
+      } else if percentage_of_max >= 0.9 && percentage_of_max < 1.0 {
+          self.messenger.send("Urgent warning: You've used up over 90% of your quota!");
+      } else if percentage_of_max >= 1.0 {
+          self.messenger.send("Error: You are over your quota!");
+      }
+  }
+}
