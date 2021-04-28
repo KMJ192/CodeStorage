be8a6bdb93d9769247d72c41f5a1c632e7649df2
KimMyeonJun <myeongjun222@naver.com>
2021-04-29 01:47:10 +0900

    codingtest

--- cpp/CodingTest/CodingTest/NumberOfProvinces.cpp
+++ cpp/CodingTest/CodingTest/NumberOfProvinces.cpp
@@ -3,46 +3,61 @@
 
 using namespace std;
 
-
+#define MAX_SIZE 200
 //1. 1번원소에서 연결되어있는 모든 간선 DFS탐색
 //2. 탐색이 되지 않은 가장 작은 원소부터 연결되어 있는 모든 원소 탐색
 //3.
 
 class Solution {
+    bool visited[MAX_SIZE] = { false, };
+    int next = 0;
+    
 public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        //배열의 길이 = 도의 수
-        int answer = isConnected.size();
-        for (int i = 0; i < isConnected.size(); i++) {
-            for(int j = i; j <isConnected[i].size(); j++){
-                /*if (j == i) {
-                    continue;
-                }*/
-                cout << isConnected[i][j] << " ";
-                if (isConnected[i][j] == 1) answer--;
+    void dfs(vector<vector<int>>& isConnected, int cell) {
+       // cout << "현재 : " << cell + 1<< endl;
+        
+        bool toggle = false;
+        for (int i = cell; i < isConnected.size(); i++) {
+            if (visited[i] == true) {
+                continue;
             }
-            cout << "\n";
+            visited[i] = true;
+            for (int j = i + 1; j < isConnected[i].size(); j++) {
+                if (isConnected[i][j] == 1) {
+                    cout << "현재 도 " << i + 1 << "와 " << j + 1 << " 연결\n";
+                    dfs(isConnected, j);
+                    toggle = true;
+                    break;
+                }
+            }
+            if (toggle == true) toggle = false;
+            else next++;
         }
-        if (answer <= 0) return 1;
+        cout << next << endl;
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int answer = 0;
+        dfs(isConnected, 0);
+        
         return answer;
     }
 };
 
 int main() {
     //vector<vector<int>> isConnected = {
-    //    {1, 1, 0},
-    //    {1, 1, 0},
-    //    {0, 0, 1}
-    //    /*{1, 0, 0},
+    //    //{1, 1, 0},
+    //    //{1, 1, 0},
+    //    //{0, 0, 1}
+    //    {1, 0, 0},
     //    {0, 1, 0},
-    //    {0, 0, 1}*/
+    //    {0, 0, 1}
     //};
 
     //총 15개
     //
     vector<vector<int>> isConnected = {
-        {1,1,0,0,0,0,0,1,0,0,0,0,0,0,0},
-        {1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {1,1,0,0,0,0,0,0,0,0,0,0,0,0,{1,1,0,0,0,0,0,1,0,0,0,0,0,0,0},0},
         {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
         {0,0,0,1,0,1,1,0,0,0,0,0,0,0,0},
         {0,0,0,0,1,0,0,0,0,1,1,0,0,0,0},
@@ -58,7 +73,7 @@ int main() {
         {0,0,0,0,0,0,0,0,0,1,0,0,0,0,1}
     };
     Solution s;
-    cout << s.findCircleNum(isConnected);
+    s.findCircleNum(isConnected);
 
 	return 0;
 }
