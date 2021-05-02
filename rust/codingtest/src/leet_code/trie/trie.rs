845805897c2c4db711368f6b908a4ec71e6e00d6
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-05-03 01:46:22 +0900

    codingtest

---
+++ rust/codingtest/src/leet_code/trie/trie.rs
@@ -0,0 +1,9 @@
+//trie는 tree의 한 종류이다
+//보통 Prefix tree digital search tree, retrieval tree라고도 부른다.
+
+fn trie(){
+
+}
+pub fn run(){
+
+}
--- rust/codingtest/src/main.rs
+++ rust/codingtest/src/main.rs
@@ -1,5 +1,7 @@
-#[path="./leet_code/bfs_dfs/topology_sort/CourseSchedulell.rs"] mod cs;
+//#[path="./leet_code/bfs_dfs/topology_sort/CourseSchedulell.rs"] mod cs;
+#[path="./leet_code/trie/trie.rs"] mod trie;
 
 fn main() {
-    cs::run();
+    //cs::run();
+    trie::run();
 }
