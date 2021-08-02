4b843c3eaf30c68241a14933ccd987aa1de834de
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-08-02 17:56:02 +0900

    codingtest

--- typescript/main.js
+++ typescript/main.js
@@ -4,7 +4,7 @@ function main() {
     //AsteroidCollision();
     var sampleArray = [1, 2, 3, 4, 5];
     var hap = sampleArray.map(function (unit) {
-        return unit + unit;
+        unit + unit;
     });
     console.log(hap);
 }
--- typescript/main.ts
+++ typescript/main.ts
@@ -3,9 +3,10 @@ import { AsteroidCollision } from "./leetCode/linked_list/AsteroidCollision";
 function main(){
     //AsteroidCollision();
     const sampleArray = [1, 2, 3, 4, 5];
-    const hap = sampleArray.map(unit => {
-        return unit + unit;
+    const hap = sampleArray.map(function (unit){
+        return unit + unit
     });
+
     console.log(hap);
 
 }
