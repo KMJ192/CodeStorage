9f3ca5eb127ed57c3a50d6885c77718e087b8285
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-08-09 23:11:46 +0900

    codingtest

---
+++ typescript/else/first.ts
@@ -0,0 +1,14 @@
+// 한사람 불들고있음
+// 다리 한번 건널때 최대 2명
+// 한사람만 불가질수있음
+// 불없으면 못지나감
+// 모든 사람이 다리 건너는 최소 시간 출력
+function run(people: number[], bridge: number){
+
+}
+
+export function firstQuestion(){
+  let people = [1, 2, 7, 10];
+  let bridge = 10;
+  run(people, bridge);
+}
