08bad9de64f13040e15466e69af745433ab00f9f
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-08-11 22:29:06 +0900

    codingtest

--- typescript/main.js
+++ typescript/main.js
@@ -1,6 +1,5 @@
 "use strict";
 exports.__esModule = true;
-var trie_1 = require("./base_ds_algorithm/trie/trie");
 function main() {
     //AsteroidCollision();
     // const sampleArray = [1, 2, 3, 4, 5];
@@ -8,6 +7,20 @@ function main() {
     //     return unit + unit
     // });
     // console.log(hap);
-    trie_1.TSRun();
+    //TSRun();
+    var object = "메뉴1";
+    var words = ["메뉴1", "메뉴2", "메뉴3", "메뉴4"];
+    // let tmp = words.filter((word: string) =>{
+    //     if(word !== '' && 
+    //         object !== ''  && 
+    //         word.includes(object) === true && 
+    //         word[0] === object[0]) {
+    //             console.log(word[0]);
+    //             console.log(object[0]);
+    //             return word;
+    //     }
+    // });
+    //console.log(tmp);
+    console.log(words.some(function (value) { return value === object; }));
 }
 main();
--- typescript/main.ts
+++ typescript/main.ts
@@ -10,6 +10,21 @@ function main(){
 
     // console.log(hap);
 
-    TSRun();
+    //TSRun();
+
+    let object = "메뉴1";
+    let words = ["메뉴1", "메뉴2", "메뉴3", "메뉴4"];
+    // let tmp = words.filter((word: string) =>{
+    //     if(word !== '' && 
+    //         object !== ''  && 
+    //         word.includes(object) === true && 
+    //         word[0] === object[0]) {
+    //             console.log(word[0]);
+    //             console.log(object[0]);
+    //             return word;
+    //     }
+    // });
+    //console.log(tmp);
+    console.log(words.some((value: string) => value === object));
 }
 main();
