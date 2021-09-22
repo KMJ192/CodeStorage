8f9edcc7f6d1d221d59ed105ca95eab8ecf22ba9
KMJ192 <myeongjun222@naver.com>
2021-09-22 18:56:26 +0900

    Update linked_list.rs

--- rust/code/base_algorithm/linked_list.rs
+++ rust/code/base_algorithm/linked_list.rs
@@ -1,4 +1,4 @@
-use std::borrow::{BorrowMut, Borrow};
+use std::fmt::Debug;
 
 #[derive(Debug)]
 struct Node<T> {
@@ -12,7 +12,7 @@ struct LinkedList<T> {
     len: usize
 }
 
-impl<T> LinkedList<T> {
+impl<T> LinkedList<T> where T: Debug {
     fn new() -> Self {
         LinkedList {
             head: None,
@@ -36,9 +36,10 @@ impl<T> LinkedList<T> {
                 // head가 없으니 새로운 node를 만든다.
                 let new_node = Node {
                     val,
-                    next: self.head.take()
+                    next: self.head.take(),
                 };
                 self.head = Some(Box::new(new_node));
+                self.len += 1;
                 return;
             }
         };
@@ -57,46 +58,92 @@ impl<T> LinkedList<T> {
     }
 
     fn pop_front(&mut self) {
-        match &self.head.take() {
-            Some(head_node) => {
-                // self.head = head_node.next.take().and_then(|a| a.next);
+        // self.head 유무 검사
+        match self.head.take() {
+            Some(head) => {
+                // self.head.next 유무 검사
+                match head.next {
+                    Some(next) => {
+                        self.head = None;
+                        self.head = Some(next);
+                    }
+                    _ => {
+                        self.head = None;
+                    }
+                }
             },
             _ => {
-                self.head = None
+                return;
             }
         }
-        self.len -= 1;
+        if self.len > 0 { self.len -= 1;}
     }
 
     fn pop_back(&mut self) {
-        if self.len == 0 { return; }
-        self.len -= 1;
+        let mut idx: usize = 1;
+        match self.head.take() {
+            Some(ref mut node) => {
+                let mut cur_node = node;
+                while let Some(ref mut next) = cur_node.next {
+                    idx += 1;
+                    if idx >= self.len {
+                        cur_node.next = None;
+                        break;
+                    }
+                    println!("{:?}", next);
+                    cur_node = next;
+                }
+            },
+            _ => {
+                return;
+            }
+        };
+
+        if self.len > 0 { self.len -= 1; }
+    }
+
+    fn display(&mut self) {
+        match self.head.take() {
+            Some(ref mut node) => {
+                println!("{:?}", node.val);
+                let mut cur_node = node;
+                while let Some(ref mut next) = cur_node.next {
+                    println!("{:?}", next.val);
+                    cur_node = next;
+                }
+            },
+            _ => {
+                return;
+            }
+        };
     }
 }
 
 pub fn run() {
-    // let mut ll = LinkedList::new();
-    // ll.push_front(1);
-    // ll.push_front(2);
-    // ll.push_back(3);
-    // println!("{:#?}", ll.head.unwrap());
+    let mut ll: LinkedList<i32> = LinkedList::new();
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.pop_back();
+    // ll.display();
+    // println!("{:#?}", ll);
 
     // let test_box = Box::new(String::from("test"));
     // let mut tmp = *test_box;
     // println!("{}", tmp);
 
-    let tmp = Some(Box::new(Node{
-        val: 1,
-        next: None
-    }));
-
-    let next = match tmp {
-        Some(a) => {
-            println!("{}", a.val);
-            Some(a)
-        },
-        None => None,
-    };
-
-    println!("{:?}", next);
+    // let tmp = Some(Box::new(Node{
+    //     val: 1,
+    //     next: None
+    // }));
+    //
+    // let next = match tmp {
+    //     Some(a) => {
+    //         println!("{}", a.val);
+    //         Some(a)
+    //     },
+    //     None => None,
+    // };
+    //
+    // println!("{:?}", next);
 }
