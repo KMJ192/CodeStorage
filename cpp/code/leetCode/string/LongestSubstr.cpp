cc07293f82a54dc6e95fe8ba16dd25d63f3724df
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-12-02 19:45:44 +0900

    test

---
+++ cpp/code/leetCode/string/LongestSubstr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+  
+public:
+  int lengthOfLongestSubstring(string s) {
+  }
+};
+
+int main() {
+  Solution s;
+
+  // s.lengthOfLongestSubstring("abcabcbb");
+  s.lengthOfLongestSubstring("ababcabcdeabcde");
+  
+  return 0;
+}
---
+++ cpp/code/tmp.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void makeTable(string s, vector<int>& table) {
+  int sLen = s.size();
+  for (int i = 1; i < sLen; i++) {
+    int j = table[i - 1];
+    while (j > 0 && s[i] != s[j]) {
+      j = table[j-1];
+    }
+    if(s[i] == s[j]) j++;
+    table[i] = j;
+  }
+}
+
+void kmp(string s, string pattern, vector<int>& table) {
+  int sLen = s.size(), patternLen = pattern.size();
+  int j = 0;
+  for (int i = 0; i < sLen; i++) {
+    while (j > 0 && s[i] != pattern[j]){
+      j = table[j - 1];
+    }
+    if (s[i] == pattern[j]) {
+      if (j == patternLen - 1) {
+        cout << i - patternLen + 1 << "\n";
+        j = table[j];
+      }
+      j++;
+    }
+  }
+}
+
+int main(){
+  using ss = pair<string, string>;
+  ss input = ss("abcdeabcdabcde", "abcde");
+  vector<int> table(input.first.size(), 0);
+  makeTable(input.first, table);
+  kmp(input.first, input.second, table);
+
+  return 0;
+}
