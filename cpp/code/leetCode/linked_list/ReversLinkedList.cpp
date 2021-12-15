aa0eccf2f75090e1056b0783a7891391b1f781a1
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-12-15 21:41:57 +0900

    Create ReversLinkedList.cpp

---
+++ cpp/code/leetCode/linked_list/ReversLinkedList.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+  stack<int> vals;
+public:
+    ListNode* reverseList(ListNode* head) {
+      if (head == nullptr) return nullptr;
+      if (head->next == nullptr) return head;
+      recursion(head);
+      ListNode* answer = new ListNode();
+      ListNode* tmp = new ListNode();
+      answer = tmp;
+
+      while (!vals.empty()) {
+        ListNode* n = new ListNode();
+        n->val = vals.top();
+        tmp->next = n;
+        tmp = tmp->next;
+        vals.pop();
+      }
+
+      answer = answer->next;
+      return answer;
+    }
+
+    void recursion(ListNode* node) {
+      vals.push(node->val);
+      if (node->next == nullptr) return;
+      recursion(node->next);
+    }
+
+    void display(ListNode* node) {
+      cout << node->val << endl;
+      if (node->next == nullptr) return;
+      display(node->next);
+    }
+};
+
+int main() {
+
+  ListNode* head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+  Solution s;
+  s.display(s.reverseList(head));
+
+  return 0;
+}
