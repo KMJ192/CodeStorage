148a744c9ca08b53e1d2ccfce47f118087c881ca
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2022-02-18 00:04:28 +0900

    rust closure currying

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,5 @@
-// #[path="./leet_code/trie/trie.rs"] mod run;
-// #[path="./syntax/macro.rs"] mod run;
-// #[path="./leet_code/trie/trie.rs"] mod trie;
-// #[path="./dp/knapsack_problem.rs"] mod knapsack;
-#[path="./dp/dijkstra.rs"] mod dijkstra;
+#[path="./syntax/closure.rs"] mod closure;
 
 fn main() {
-  // dijkstra::dijkstra_build();
-  println!("{}", std::u32::MAX);
+  closure::closure_run();
 }
