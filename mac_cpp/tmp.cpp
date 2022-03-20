3fd1fe750695df8bbaf7d46c08f65dbd51428c58
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2022-03-20 17:41:42 +0900

    tmp

--- mac_cpp/tmp.cpp
+++ mac_cpp/tmp.cpp
@@ -1,31 +1,61 @@
+/**
+ * @file tmp.cpp
+ * @author MyeongJunKim (myeongjun222@github.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-20
+ * 
+ * @copyright Copyright (c) 2022
+ * Longest Valid Parentheses
+ * 괄호 '(' , ')'로 이루어진 문자열을 주고 가장 긴 유효한 괄호 형식으로 된 문자열의 길이를 반ㅏㄴ
+ * input : string type s
+ * constraint
+ *  - 0 ≤ s.length() ≤ 3 * 10^4
+ *  - s[i] == '(' or ')'
+ * 
+ * solution
+   ) ( ) ( ) )
+   0 1 2 3 4 5
+   st
+   0 -1   -> 0
+   1 0, 1 -> 0
+   2 0    -> 2
+   3 0, 3 -> 2
+   4 0    -> 4
+   5 0    -> 4
+
+*/
+
 #include <iostream>
+#include <stack>
+#include <algorithm>
 #include <unistd.h>
 
 using namespace std;
 
 class Solution {
 public:
-  string convert(string s, int numRows) {
-    if (numRows <= 1) return s;
-    string answer = "";
-    int sLen = s.length();
-    for (int i = 0; i < numRows; i++) {
-      int increment = 2 * (numRows - 1);
-      for (int j = i; j < sLen; j += increment) {
-        answer += s[j];
-        if (i > 0 && i < (numRows - 1) && (j + increment - 2 * i) < sLen) {
-          answer += s[j + increment - 2 * i];
+    int longestValidParentheses(string s) {
+      int answer = 0;
+      stack<int> st;
+      st.push(-1);
+
+      for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(') st.push(i);
+        else {
+          st.pop();
+          if (st.empty()) st.push(i);
+          else answer = max(answer, i - st.top());
         }
+        cout << i << ", " << answer << endl;
       }
+
+      return answer;
     }
-    return answer;
-  }
 };
 
 int main() {
-  string s = "PAYPALISHIRING";
-  Solution so;
-  cout << so.convert(s, 3);
-  
+  Solution s;
+  cout << s.longestValidParentheses(")()())") << endl;
   return 0;
 }
