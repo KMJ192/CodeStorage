e7aa32c2ddd758341d41733e59698052b9cddada
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2022-03-25 23:24:05 +0900

    Update cow2.rs

--- rust/src/syntax/cow2.rs
+++ rust/src/syntax/cow2.rs
@@ -1,3 +1,11 @@
+// cow
 pub fn cow2() {
 
+  // string을 만드는 방법
+  let string_1 = String::from("Hello"); // From trait
+  let string_2 = "Hello".to_string(); // Display trait
+  let string_3: String = "Hello".into(); // From
+  let string_4 = "Hello".to_owned(); // ToOwned trait : str -> String
+
+  println!("{} {} {} {}", string_1, string_2, string_3, string_4);
 }
