3ad68fedf8a889784ef181bf681ce396064c74b5
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2022-03-27 20:17:19 +0900

    Update tmp.cpp

--- mac_cpp/tmp.cpp
+++ mac_cpp/tmp.cpp
@@ -1,61 +1,56 @@
 /**
+ * https://leetcode.com/problems/reverse-nodes-in-k-group/
  * @file tmp.cpp
- * @author MyeongJunKim (myeongjun222@github.com)
+ * @author your name (you@domain.com)
  * @brief 
  * @version 0.1
- * @date 2022-03-20
+ * @date 2022-03-27
  * 
  * @copyright Copyright (c) 2022
- * Longest Valid Parentheses
- * 괄호 '(' , ')'로 이루어진 문자열을 주고 가장 긴 유효한 괄호 형식으로 된 문자열의 길이를 반ㅏㄴ
- * input : string type s
- * constraint
- *  - 0 ≤ s.length() ≤ 3 * 10^4
- *  - s[i] == '(' or ')'
  * 
- * solution
-   ) ( ) ( ) )
-   0 1 2 3 4 5
-   st
-   0 -1   -> 0
-   1 0, 1 -> 0
-   2 0    -> 2
-   3 0, 3 -> 2
-   4 0    -> 4
-   5 0    -> 4
-
-*/
+ */
 
 #include <iostream>
-#include <stack>
-#include <algorithm>
 #include <unistd.h>
 
 using namespace std;
 
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-    int longestValidParentheses(string s) {
-      int answer = 0;
-      stack<int> st;
-      st.push(-1);
-
-      for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '(') st.push(i);
-        else {
-          st.pop();
-          if (st.empty()) st.push(i);
-          else answer = max(answer, i - st.top());
-        }
-        cout << i << ", " << answer << endl;
-      }
-
-      return answer;
+    ListNode* reverseKGroup(ListNode* head, int k) {
+      
+      return head;
     }
 };
 
 int main() {
+  ListNode* head = new ListNode(
+    1, 
+    new ListNode(
+      2, 
+      new ListNode(
+        3, 
+        new ListNode(
+          4, 
+          new ListNode(
+            5
+          )
+        )
+      )
+    )
+  );
+
   Solution s;
-  cout << s.longestValidParentheses(")()())") << endl;
+
+  s.reverseKGroup(head, 3);
+
   return 0;
 }
