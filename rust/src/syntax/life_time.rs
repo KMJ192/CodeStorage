08889990eab90f940b3996b8904e8473d1e7f3d0
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2022-03-31 23:43:46 +0900

    Update life_time.rs

--- rust/src/syntax/life_time.rs
+++ rust/src/syntax/life_time.rs
@@ -5,8 +5,16 @@ struct Book<'a> {
   name: &'a str,
 }
 
-fn print_thing<T: Display>(input: T) {
-  
+struct Adventurer<'a> {
+  name: &'a str,
+  hit_points: u32,
+}
+
+impl Adventurer<'_> {
+  fn take_damage(&mut self) {
+    self.hit_points -= 20;
+    println!("{} has {} hit points left!", self.name, self.hit_points);
+  }
 }
 
 pub fn life_time() {
