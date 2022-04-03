8e1889bc8522c1ab0362b448668e87c06ff65bf4
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2022-04-03 22:17:03 +0900

    Update tmp.cpp

--- mac_cpp/tmp.cpp
+++ mac_cpp/tmp.cpp
@@ -1,6 +1,6 @@
 /**
- * https://leetcode.com/problems/reverse-nodes-in-k-group/
- * @file tmp.cpp
+ * https://leetcode.com/problems/shortest-palindrome/
+ * @file ShortestPalindrom.cpp
  * @author your name (you@domain.com)
  * @brief 
  * @version 0.1
@@ -8,49 +8,67 @@
  * 
  * @copyright Copyright (c) 2022
  * 
+ * kmp solution
+ * 
  */
 
 #include <iostream>
+#include <vector>
 #include <unistd.h>
 
 using namespace std;
 
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
 class Solution {
 public:
-    ListNode* reverseKGroup(ListNode* head, int k) {
-      
-      return head;
+  string shortestPalindrome(string s) {
+    int sLen = s.length();
+    string answer(s);
+    reverse(answer.begin(), answer.end());
+    string tmp = s + "#" + answer;
+    // tmp = abcd#bcda
+    int maxLen = tmp.length();
+    vector<int> f(maxLen, 0);
+
+    // abcd
+    /*
+      a b c d
+
+    i     ↓
+      a b c d # b c d a
+    t ↑
+
+    f 0 0 0 0 0 0 0 0 0
+    */
+
+    int i = 1;
+    while (i < maxLen) {
+      int t = f[i - 1];
+      while (tmp[i] != tmp[t]) {
+        if (t > 0) {
+          t = f[t - 1];
+        } else {
+          f[i] = 0;
+          i++;
+          continue;
+        }
+      }
+      if (tmp[i] == tmp[t]) {
+        t++;
+        f[i] = t;
+      }
+      i++;
     }
+
+    return answer.substr(0, sLen - f[maxLen - 1]) + s;
+
+    return answer;
+  }
 };
 
 int main() {
-  ListNode* head = new ListNode(
-    1, 
-    new ListNode(
-      2, 
-      new ListNode(
-        3, 
-        new ListNode(
-          4, 
-          new ListNode(
-            5
-          )
-        )
-      )
-    )
-  );
-
   Solution s;
 
-  s.reverseKGroup(head, 3);
+  cout << s.shortestPalindrome("abcd") << endl;
 
   return 0;
 }
