7291069bfc574eecb27c87085d93bc2b3ec4774c
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2022-04-05 22:41:27 +0900

    Update iterator.rs

--- rust/src/syntax/iterator.rs
+++ rust/src/syntax/iterator.rs
@@ -1,87 +1,105 @@
 #[derive(Debug)]
 struct Shoe {
-    size: u32,
-    style: String,
+	size: u32,
+	style: String,
 }
 
 fn my_shoes(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
-    shoes.into_iter()
-        .filter(|s| s.size == shoe_size)
-        .collect()
+	shoes.into_iter()
+		.filter(|s| s.size == shoe_size)
+		.collect()
 }
 
 #[derive(Debug)]
 struct Counter {
-    count: u32,
+	count: u32,
 }
 
 impl Counter {
-    fn new() -> Counter {
-        Counter { count: 0 }
-    }
+	fn new() -> Counter {
+		Counter { count: 0 }
+	}
 }
 
 impl Iterator for Counter {
-    type Item = u32;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.count += 1;
-        if self.count < 6 {
-            Some(self.count)
-        } else {
-            None
-        }
-    }
+	type Item = u32;
+	fn next(&mut self) -> Option<Self::Item> {
+		self.count += 1;
+		if self.count < 6 {
+			Some(self.count)
+		} else {
+			None
+		}
+	}
 }
 
 fn vec_iterator() {
-    let mut count = Counter::new();
-    if let Some(N) = count.next() {
-        println!("{}", N);
-    }
-    println!("{:?}", count.next());
-    println!("{:?}", count.next());
-    println!("{:?}", count.next());
-    println!("{:?}", count.next());
-    println!("{:?}", count.next());
+	let mut count = Counter::new();
+	if let Some(N) = count.next() {
+		println!("{}", N);
+	}
+	println!("{:?}", count.next());
+	println!("{:?}", count.next());
+	println!("{:?}", count.next());
+	println!("{:?}", count.next());
+	println!("{:?}", count.next());
 
-    let sum: u32 = Counter::new().zip(Counter::new().skip(1))
-        .map(|(a, b)| a * b)
-        .filter(|x| x % 3 == 0)
-        .sum();
+	let sum: u32 = 
+		Counter::new().zip(Counter::new().skip(1))
+			.map(|(a, b)| a * b)
+			.filter(|x| x % 3 == 0)
+			.sum();
 
-    println!("{:#?}", sum);
+	println!("{:#?}", sum);
 
+	// let v1 = vec![1, 2, 3];
+	// let mut v1_iter = v1.iter();
 
-    // let v1 = vec![1, 2, 3];
-    // let mut v1_iter = v1.iter();
+	// println!("{:?}", v1_iter);
+	// println!("{:?}", v1_iter.next());
+	// println!("{:?}", v1_iter.next());
+	// println!("{:?}", v1_iter.next());
+	// println!("{:?}", v1_iter.next());
+	// println!("{:?}", v1_iter);
 
-    // println!("{:?}", v1_iter);
-    // println!("{:?}", v1_iter.next());
-    // println!("{:?}", v1_iter.next());
-    // println!("{:?}", v1_iter.next());
-    // println!("{:?}", v1_iter.next());
-    // println!("{:?}", v1_iter);
+	// let total: i32 = v1_iter.sum();
+	// println!("{}", total);
+	// let v2 = v1.iter().map(|x| x + 1);
+	// println!("{:?}", v2);
+	// let v3: Vec<i32> = v1.iter().map(|x| x + 1).collect();
+	// println!("{:?}", v3);
 
-    // let total: i32 = v1_iter.sum();
-    // println!("{}", total);
-    // let v2 = v1.iter().map(|x| x + 1);
-    // println!("{:?}", v2);
-    // let v3: Vec<i32> = v1.iter().map(|x| x + 1).collect();
-    // println!("{:?}", v3);
-
-    // let shoes: Vec<Shoe> = vec![
-    //     Shoe { size: 240, style: String::from("스니커즈") },
-    //     Shoe { size: 255, style: String::from("샌들") },
-    //     Shoe { size: 260, style: String::from("디올 B23 하이탑 스니커즈 화이트") },
-    //     Shoe { size: 235, style: String::from("단화") },
-    //     Shoe { size: 260, style: String::from("발렌시아가 트리플s 트레이너") },
-    // ];
-    //
-    // let mine = my_shoes(shoes, 260);
-    //
-    // println!("{:?}", mine);
+	// let shoes: Vec<Shoe> = vec![
+	//     Shoe { size: 240, style: String::from("스니커즈") },
+	//     Shoe { size: 255, style: String::from("샌들") },
+	//     Shoe { size: 260, style: String::from("디올 B23 하이탑 스니커즈 화이트") },
+	//     Shoe { size: 235, style: String::from("단화") },
+	//     Shoe { size: 260, style: String::from("발렌시아가 트리플s 트레이너") },
+	// ];
+	//
+	// let mine = my_shoes(shoes, 260);
+	//
+	// println!("{:?}", mine);
 }
 
-pub fn run() {
-    vec_iterator();
+pub fn iterator() {
+  // vec_iterator();
+
+	let num_vec = vec![2, 4, 6];
+
+	// let double_vec = 
+	// 	num_vec
+	// 		.iter()
+	// 		.map(|number| number * 2)
+	// 		.map(|number| number * 3)
+	// 		.map(|number| number * 4)
+	// 		.collect::<Vec<i32>>();
+
+	num_vec
+		.iter() // 2, 4, 6
+		.enumerate() // (0, 2), (1, 4), (2, 6)
+		// .for_each(|(i, number)| { // (index, value)
+		.for_each(|tuple| {
+			println!("The number is index {} is {}", tuple.0, tuple.1);
+		});
 }
