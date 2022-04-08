75065e50af2aa13e65d7c43abedd64cb3594a714
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2022-04-08 22:49:48 +0900

    Create box.rs

---
+++ rust/src/syntax/box.rs
@@ -0,0 +1,19 @@
+struct SomeStruct {
+  name: String,
+  number: u8,
+  data: Box<[u8; 10000]>,
+}
+
+pub fn _box() {
+  let my_struct = SomeStruct {
+    name: "Hey there".to_string(),
+    number: 0,
+    data: Box::new([9; 10000])
+  };
+
+  // Box를 이용할 경우 40byte, 아닐 경우 10032bytes
+  // println!("The struct is {} bytes", std::mem::size_of_val(&my_struct));
+  let my_box = Box::new(9);
+  println!("{my_box}");
+  println!("{}", *my_box);
+}
