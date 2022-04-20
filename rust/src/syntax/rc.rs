72799b17588994b470a9370d9a0fffcc0eb5f123
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2022-04-20 22:58:04 +0900

    Update rc.rs

--- rust/src/syntax/rc.rs
+++ rust/src/syntax/rc.rs
@@ -20,15 +20,24 @@ impl MyStruct {
   }
 }
 
-
-
-
 fn takes_a_string(input: Rc<String>) {
   println!("{input}");
 }
 
 fn also_takes_a_string(input: Rc<String>) {
   println!("{input}");
+}
+
+struct City {
+  name: String,
+  population: u32,
+  history: Rc<String>,
+}
+
+#[derive(Debug)]
+struct CityData {
+  names: Vec<String>,
+  histories: Vec<Rc<String>>
 }
 
 pub fn rc() {
@@ -41,4 +50,24 @@ pub fn rc() {
   takes_a_string(Rc::clone(&my_string)); // Associated function syntax, rc rust fully qualified
   also_takes_a_string(Rc::clone(&my_string));
 
+  let calgary = City {
+    name: "Calgary".to_string(),
+    population: 1_336_000,
+    history: Rc::new("history".to_string()),
+  };
+
+  let canada_cities =CityData {
+    names: vec![calgary.name],
+    histories: vec![Rc::clone(&calgary.history)]
+  };
+
+  println!("Calgary's history is: {}", calgary.history);
+
+  // strong_count
+  // 소유권자가 하나라도 있다면 Rc drop을 할 수 없도록 함.
+  println!("Data has {} owners", Rc::strong_count(&calgary.history));
+
+  // weak_count
+  // 소유권자가 있더라고 Rc drop을 할 수 있도록 함.
+  println!("Data has {} owners", Rc::weak_count(&calgary.history));
 }
