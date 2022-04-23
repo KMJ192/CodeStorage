b750b438b75bcd46e9a6e4013351edc3438bf5a2
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2022-04-23 22:32:10 +0900

    Update tmp.cpp

--- mac_cpp/tmp.cpp
+++ mac_cpp/tmp.cpp
@@ -1,74 +1,66 @@
 /**
- * https://leetcode.com/problems/shortest-palindrome/
- * @file ShortestPalindrom.cpp
- * @author your name (you@domain.com)
+ * https://leetcode.com/problems/edit-distance/
+ * @file editDistance.cpp
+ * @author MyeongJun Kim (you@domain.com)
  * @brief 
  * @version 0.1
- * @date 2022-03-27
+ * @date 2022-04-23
  * 
  * @copyright Copyright (c) 2022
  * 
- * kmp solution
- * 
+ * input: {string} word1, {string} word2
+ * output: int
+ * constraint
+ *   - 0 <= word1.length, word2.length <= 500
+ *   - word1, word2 => lowercase english letters
+ * word1, word2가 주어진다.
+ * word1에서 word2로 만들 수 있는 최소한의 작업숫자를 구할것
+ * word에 대한 가능한 작업
+ * Insert a character
+ * Delete a character
+ * Replace a character
  */
 
 #include <iostream>
-#include <vector>
-#include <unistd.h>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
+private:
+  int dp[501][501];
 public:
-  string shortestPalindrome(string s) {
-    int sLen = s.length();
-    string answer(s);
-    reverse(answer.begin(), answer.end());
-    string tmp = s + "#" + answer;
-    // tmp = abcd#bcda
-    int maxLen = tmp.length();
-    vector<int> f(maxLen, 0);
+  int minDistance(string word1, string word2) {
+    // dp를 -1로 채워줌
+    memset(dp, -1, sizeof(dp));
+    return calc(word1, word2, word1.size() - 1, word2.size() - 1);
+  }
 
-    // abcd
-    /*
-      a b c d
-
-    i     ↓
-      a b c d # b c d a
-    t ↑
-
-    f 0 0 0 0 0 0 0 0 0
-    */
-
-    int i = 1;
-    while (i < maxLen) {
-      int t = f[i - 1];
-      while (tmp[i] != tmp[t]) {
-        if (t > 0) {
-          t = f[t - 1];
-        } else {
-          f[i] = 0;
-          i++;
-          continue;
-        }
-      }
-      if (tmp[i] == tmp[t]) {
-        t++;
-        f[i] = t;
-      }
-      i++;
+  int calc(string &word1, string &word2, int i, int j) {
+    if (i < 0 || j < 0) {
+      // cout << "i : " << i << ", j : " << j << endl;
+      if (i < 0 && j < 0) return 0;
+      if (i < 0) return j + 1;
+      return i + 1;
     }
 
-    return answer.substr(0, sLen - f[maxLen - 1]) + s;
+    if (dp[i][j] != -1) return dp[i][j];
 
-    return answer;
+    if (word1[i] == word2[j]) {
+      dp[i][j] = calc(word1, word2, i - 1, j - 1);
+    } else {
+      dp[i][j] = 1 + min({calc(word1, word2, i - 1, j), calc(word1, word2, i - 1, j - 1), calc(word1, word2, i, j - 1)});
+    }
+
+    cout << "i : " << i << ", j : " << j << endl;
+    return dp[i][j];
   }
 };
 
 int main() {
   Solution s;
 
-  cout << s.shortestPalindrome("abcd") << endl;
+  cout << s.minDistance("intention", "execution") << endl;
 
   return 0;
 }
