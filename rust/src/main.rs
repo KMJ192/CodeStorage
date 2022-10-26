1fb3ec766e2d9bdbee5ae9265adb083d881361cc
MyeongJunKim <myeongjun222@naver.com>
2022-10-26 23:07:35 +0900

    test

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,9 @@
 // #[path="./syntax/async.rs"] mod async_fn;
-#[path="./base_algorithm/page_replacement/lfu_cache.rs"] mod lfu_cache;
+// #[path="./base_algorithm/page_replacement/lfu_cache.rs"] mod lfu_cache;
+
+pub mod thread;
+use thread::thread;
 
 fn main() {
-  lfu_cache::run();
+  thread();
 }
---
+++ rust/src/thread/thread.rs
@@ -0,0 +1,6 @@
+use std::thread;
+use std::time::Duration;
+
+fn thread() {
+  println!("test");
+}
